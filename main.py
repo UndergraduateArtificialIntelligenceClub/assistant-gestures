e2346eaea8d4ac6a444d6f20c4597e9addd34798
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2019-01-19 22:07:20 -0700

    added python file execution

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import json
 import os
+# import pynput
+from pynput.keyboard import Key, Controller
 
 config = None
 
 # opens config.json and saves it to a variable
 with open('config.json') as f:
-    config = json.load(f)
+    config = json.load(f)["gestures"]
 
-gesture = "3_fingers"
-script = config["gestures"][gesture]["action"]
-# print(script)
-os.system(script)
+gesture = "2_fingers"
+
+# type = config[gesture]["type"]
+type = "media"
+
+if (type == "script"):
+    script = config[gesture]["action"]
+    os.system(script)
+elif (type == "python"):
+    python_path = config[gesture]["action"]
+    os.system("python " + "'" + python_path + "'")
+elif (type == 'media'):
+    keyboard = Controller()
+    print(keyboard)
